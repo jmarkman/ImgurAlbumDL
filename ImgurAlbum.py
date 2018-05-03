50ec25cce04cead874d788e09051f24c7ccbd1db
Jonathan Markman <jmarkman446@gmail.com>
2018-05-02 23:57:02 -0400

    initial commit - created gui - created imgur album api interaction class

---
+++ ImgurAlbum.py
@@ -0,0 +1,65 @@
+"""Class for interacting with the Imgur API"""
+import requests
+import os
+import json
+
+class ImgurJSON:
+    """Provides interaction with JSON returned from the Imgur API"""
+    def __init__(self, url):
+        self.web_url = url
+        self.json_url = self.__buildJSONUrl()
+        self.album_json = None
+        self.auth_key = self.__loadClientID()
+
+    def __loadClientID(self):
+        self.auth_path = os.path.join(os.path.dirname(__file__), 'auth.json')
+        with open(self.auth_path) as authFile:
+            authDict = json.loads(authFile.read())     
+        return authDict
+
+    def __buildJSONUrl(self):
+        slash_location = self.web_url.rindex('/')
+        album_id = self.web_url[slash_location + 1:]
+        api_url = "https://api.imgur.com/3/album/{0}/images".format(album_id)
+        return api_url
+
+    def getAlbumJSON(self):
+        """Fetches the JSON for the album from the url provided by the constructor"""
+        response = requests.get(self.json_url, headers=self.auth_key)
+        self.album_json = response.json()
+
+    def isAccessible(self):
+        """Determines if the API query returned successfully"""
+        if self.album_json is not None:
+            album_status = str(self.album_json["success"])
+        if album_status == 'True':
+            api_success = True
+        else:
+            api_success = False
+        return api_success
+
+    def downloadImages(self, filepath):
+        image_urls = self.__getImageList()
+        for url in image_urls:
+            last_slash = str(url).rindex('/')
+            filename = str(url)[last_slash + 1:]
+            album_name = self.json_url[last_slash + 1]
+            filepath = r"{0}\{1}".format(filepath, album_name)
+            with open(r"{0}\{1}".format(filepath, filename), "wb") as image:
+                image.write(requests.get(url).content)
+                
+    def __getImageList(self):
+        """Retrieves the URLs from the returned JSON"""
+        if self.album_json is not None:
+            image_data = self.album_json["data"]
+        url_list = []
+        for image in image_data:
+            if image["link"] is not None or image["link"] != "":
+                url_list.append(image["link"])
+        return url_list
+
+
+
+    
+
+    
---
+++ imgurDL.py
@@ -0,0 +1,67 @@
+"""GUI for Imgur Album Downloader"""
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+import os
+import re
+import ImgurAlbum
+
+class GUI:
+    """Object-oriented GUI building"""
+    def __init__(self):
+        """Window initializer"""
+        # Window generation
+        self.main_window = tk.Tk()
+        self.album_url = tk.StringVar()
+        self.main_window.title("Imgur Album Downloader")
+        self.main_window.resizable(0, 0)
+        self.createWidgets()
+
+        # Main loop
+        self.main_window.mainloop()
+
+    def createWidgets(self):
+        self.album_input_lbl = tk.Label(self.main_window, text="Album")
+        self.album_input_lbl.grid(column=0, row=0)
+        
+        self.album_input = tk.Entry(self.main_window, textvariable=self.album_url, width=50)
+        self.album_input.grid(column=1, row=0)
+
+        self.album_get_btn = tk.Button(self.main_window, text="Get Album", command=self.getAlbum)
+        self.album_get_btn.grid(column=2, row=0)
+
+    def verifyURL(self):
+        match = re.search("imgur.com", self.album_url.get())
+        if match:
+            is_valid_url = True
+        else:
+            is_valid_url = False
+
+        return is_valid_url
+
+    def getAlbum(self):
+        correct_url = self.verifyURL()
+        if correct_url:
+            url = self.album_url.get()
+            imgurAlbum = ImgurAlbum.ImgurJSON(url)
+            try:
+                imgurAlbum.getAlbumJSON()
+                if imgurAlbum.isAccessible():
+                    imgurAlbum.downloadImages(os.getcwd())
+                else:
+                    messagebox.showwarning(message="Something happened")
+            except:
+                raise
+        else:
+            messagebox.showwarning(message="The url supplied is not valid!")
+            self.album_input.delete(0, 'end')
+
+def launch():
+    GUI()
+
+if __name__ == '__main__':
+    launch()
+
+
+
+
